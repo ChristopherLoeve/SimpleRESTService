b0a5cfd36d855a71859d2bd8ce4bc662a5500242
Christopher <38313657+NattyPew@users.noreply.github.com>
2021-09-25 12:14:57 +0200

    Add project files.

---
+++ SimpleRESTService/Controllers/FootballPlayerController.cs
@@ -0,0 +1,97 @@
+﻿using FootballPlayerLib;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleRESTService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FootballPlayerController : Controller
+    {
+        private static int nextId = 4;
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(FootballPlayerManager.Get());
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+
+                return Ok(FootballPlayerManager.Get(id));
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpPost("{name}/{price}/{shirtNumber}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Create(string name, int price, int shirtNumber)
+        {
+            try
+            {
+                FootballPlayer player = new FootballPlayer(nextId++, name, price, shirtNumber);
+                return Ok(FootballPlayerManager.Create(player));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                return Ok(FootballPlayerManager.Delete(id));
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Update(int id, [FromBody] FootballPlayer player)
+        {
+            try
+            {
+                return Ok(FootballPlayerManager.Update(id, player));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+
+    }
+}
---
+++ SimpleRESTService/FootballPlayerManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FootballPlayerLib;
+
+namespace SimpleRESTService
+{
+    public class FootballPlayerManager
+    {
+        private static List<FootballPlayer> _players = new List<FootballPlayer>() {
+            new FootballPlayer(0, "Henrik", 1000, 15),
+            new FootballPlayer(1, "Peter", 800, 4),
+            new FootballPlayer(2, "Lars", 1250, 9),
+            new FootballPlayer(3, "Daniel", 25, 16)
+        };
+
+        public static List<FootballPlayer> Get()
+        {
+            return _players;
+        }
+
+        public static FootballPlayer Get(int id)
+        {
+            try 
+            { 
+                return _players.Find(p => p.Id == id); 
+            }
+            catch (ArgumentException e) 
+            {
+                throw new ArgumentException($"FootballPlayer with {id} not found");
+            }        
+        }
+
+        public static bool Create(FootballPlayer player)
+        {
+            try 
+            {
+                _players.Add(player);
+                return true;
+            }
+            catch (Exception e) {
+                return false;
+            }            
+        }
+
+        public static bool Update(int id, FootballPlayer player)
+        {
+            try 
+            {
+                FootballPlayer currentPlayer = Get(id);
+                currentPlayer.Id = player.Id;
+                currentPlayer.Name = player.Name;
+                currentPlayer.Price = player.Price;
+                currentPlayer.ShirtNumber = player.ShirtNumber;
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public static FootballPlayer Delete(int id)
+        {
+            try 
+            {
+                FootballPlayer player = Get(id);
+                _players.Remove(player);
+                return player;
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException($"FootballPlayer with {id} not found");
+            }
+        }
+    }
+}
